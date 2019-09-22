e62d6ffee79588ee0bd94c3e90c9cdbc123aa7fa
srggrhc <srg.grch@gmail.com>
2019-09-22 22:36:09 +0700

    1lab on c#, some improves in clion 1lab

--- 1st-subgroup/Sidorenko,Grechishnikov/1lab-clion/main.cpp
+++ 1st-subgroup/Sidorenko,Grechishnikov/1lab-clion/main.cpp
@@ -1,27 +1,58 @@
 #include <iostream>
 #include "MyList.h"
+
+void performOperation(int menuItem);
+
 using namespace std;
 
-int nl1, nl2;
+void printMenu() {
+    cout << "1. Add element" << endl;
+    cout << "2. Add element to position" << endl;
+    cout << "3. Show list" << endl;
+}
 
 
-void main() {
-    List<int> l1, l2, lout;
-    int n = 0, a;
 
-    cout << "Enter first List" << endl;
-    cout << "Number of elements: ";
-
-    cin >> nl1;
-    for (int i = 0; i < nl1; i++) {
-        cout << i + 1 << " elemet: ";
-        cin >> a;
-        l1.add(a);
+void performOperation(int menuItem, List<int> list) {
+    switch (menuItem) {
+        case 1: {
+            int elem = 0;
+            cout << "Add number: ";
+            cin >> elem;
+            list.add(elem);
+            break;
+        }
+        case 3:{
+            for (int i = 0; i < list.getSize(); i++) {
+                cout << list.get(i) << " ";
+            }
+            break;
+        }
+        default: {
+            break;
+        }
     }
+}
+
+int main() {
+    setlocale(0, "");
+
+    List<int> l1, l2, lout;
+    int n = 0, a, listSize = 0;
+
+    int menuItem = -1;
+
+    printMenu();
+
+    while (menuItem != 0) {
+        cout << ">>> ";
+        cin >> menuItem;
+        performOperation(menuItem, l1);
+        l1.get(0);
+    }
+
     l1.del(1);
 
-    for (int i = 0; i < l1.getSize(); i++) {
-        cout << l1.get(i) << endl;
-    }
     system("pause");
+    return 0;
 }
---
+++ 1st-subgroup/Sidorenko,Grechishnikov/1lab-cs/SAOD1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAOD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(50, 0);
+
+            for (int i = 0; i < list.GetSize(); i++)
+                Console.WriteLine(list.Get(i));
+            Console.ReadLine();
+        }
+    }
+}
